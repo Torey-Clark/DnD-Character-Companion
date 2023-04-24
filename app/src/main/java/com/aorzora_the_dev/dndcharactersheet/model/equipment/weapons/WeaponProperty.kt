d2500d6d6a355b0e50e80f822b2936ffa7830f2f
Torey Clark <clarktorey@gmail.com>
2023-04-23 22:08:26 -0500

    Defined weapon properties

---
+++ app/src/main/java/com/aorzora_the_dev/dndcharactersheet/model/equipment/weapons/WeaponProperty.kt
@@ -0,0 +1,15 @@
+package com.aorzora_the_dev.dndcharactersheet.model.equipment.weapons
+
+enum class WeaponProperty {
+    AMMUNITION,
+    FINESSE,
+    HEAVY,
+    LIGHT,
+    LOADING,
+    RANGE,
+    REACH,
+    SPECIAL,
+    THROWN,
+    TWO_HANDED,
+    VERSATILE,
+}
