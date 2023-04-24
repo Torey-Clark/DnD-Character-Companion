3766a4626ba69428b5346785ee8c74148ef79ad6
Torey Clark <clarktorey@gmail.com>
2023-04-23 22:06:47 -0500

    Defined damage types

---
+++ app/src/main/java/com/aorzora_the_dev/dndcharactersheet/model/DamageType.kt
@@ -0,0 +1,17 @@
+package com.aorzora_the_dev.dndcharactersheet.model
+
+enum class DamageType {
+    BLUDGEONING,
+    PIERCING,
+    SLASHING,
+    ACID,
+    POISON,
+    FIRE,
+    COLD,
+    FORCE,
+    LIGHTNING,
+    THUNDER,
+    NECROTIC,
+    PSYCHIC,
+    RADIANT,
+}
